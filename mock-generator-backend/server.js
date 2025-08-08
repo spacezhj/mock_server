d64cc968f2ad4298ab33db724b5f0e9c513a80db
zhj <spacezhj@foxmail.com>
2025-08-08 10:50:15 +0800

    refactor(mock-generator-backend): 优化项目结构和配置
    - 新增 dist 目录下的 index.html 文件，用于 Vite + Vue 项目的入口
    - 更新 .gitignore 文件，排除 dist 目录但包含 dist-ssr
    - 修改 vercel.json 中的路径配置，使用相对路径并指向正确位置

--- mock-generator-backend/server.js
+++ mock-generator-backend/server.js
@@ -10,8 +10,15 @@ const PORT = process.env.PORT || 3000;
 // 将express应用挂载到json-server应用上
 jsonServerApp.use(expressApp);
 
-jsonServerApp.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-  console.log(`Mock API available at http://localhost:${PORT}/mock`);
-  console.log(`Main API available at http://localhost:${PORT}/api`);
-});
+console.log(process.env.VERCEL)
+// 如果不是在 Vercel 环境中，则启动服务器
+if (!process.env.VERCEL) {
+  jsonServerApp.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    console.log(`Mock API available at http://localhost:${PORT}/mock`);
+    console.log(`Main API available at http://localhost:${PORT}/api`);
+  });
+}
+// 导出默认函数供 Vercel 使用
+export default jsonServerApp;
+
